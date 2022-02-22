e024575dfbea0f7cbbe6cd2140b215edaabd228a
azheng2639 <azheng2639@gmail.com>
2022-02-22 12:58:23 -0500

    CS2 BC FALL 2021

--- cs2/units/unit4/Vector/Test.java
+++ cs2/units/unit4/Vector/Test.java
@@ -3,5 +3,10 @@ class Test
 	public static void main(String[] args)
 	{
 		Vector v = new Vector();
+		v.add(Integer.valueOf(0));
+		v.add(Integer.valueOf(4));
+		v.add(Integer.valueOf(3));
+		v.add(Integer.valueOf(2));
+		v.add(Integer.valueOf(7));
 	}
 }
